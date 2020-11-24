01e63cc1bd9ec48a53e9e392329c85c261330e9b
Neutronic <kmurphytown@gmail.com>
2020-11-23 20:15:48 -0800

    add missing alt property

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 
 function Tooltip(props) {
+    const tooltip = useRef();
+    let width = useRef(0);
+    let height = useRef(0);
+    let x = props.x;
+    let y = props.y;
+    useEffect(() => {
+        width.current = tooltip.current.clientWidth;
+        height.current = tooltip.current.clientHeight;
+    }, [tooltip])
+    if (width.current + props.x > document.documentElement.clientWidth) {
+        x = document.documentElement.clientWidth - width.current;
+    }
+    if (height.current + props.y > document.documentElement.clientHeight) {
+        y = document.documentElement.clientHeight - height.current;
+    }
     return (
-        <div className="tooltip" style={{transform: "translate(" + props.x + "px," + props.y + "px)"}}>
+        <div className="tooltip" style={{transform: "translate(" + x + "px," + y+ "px)"}} ref={tooltip}>
             {props.title}
             <br/>
             <div className="info">{props.children}</div>
