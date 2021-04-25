35a1bc258d739fe881db3859cf718660bfaa5268
DaPigGuy <mcpepig123@gmail.com>
2021-04-25 02:03:18 -0700

    Removed overwrite for Halikar Wraith

--- update_items.js
+++ update_items.js
@@ -217,8 +217,6 @@ const itemBlacklist = ["Prisma Machete"];
 	//TODO: Remove this
 	if (!newItems["AW_GUN"]["Prisma Dual Decurions"])
 		newItems["AW_GUN"]["Prisma Dual Decurions"] = { mr: 10 };
-	if (!newItems["MELEE"]["Halikar Wraith"])
-		newItems["MELEE"]["Halikar Wraith"] = { mr: 13 };
 
 	let additions = [];
 	let deletions = [];
