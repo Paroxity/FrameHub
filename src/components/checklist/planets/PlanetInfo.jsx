05e98d8c9d7b480227ec68702db4ff71ad698a5e
DaPigGuy <mcpepig123@gmail.com>
2021-07-24 15:39:43 -0700

    Include planet junctions in planet mastery info

--- src/components/checklist/planets/PlanetInfo.jsx
+++ src/components/checklist/planets/PlanetInfo.jsx
@@ -2,28 +2,44 @@ import PropTypes from "prop-types";
 import shallow from "zustand/shallow";
 import { useStore } from "../../../hooks/useStore";
 import nodes from "../../../resources/nodes.json";
+import { junctionsToXP } from "../../../utils/mastery-rank";
+import { planetsWithJunctions } from "../../../utils/nodes";
 import BaseCategoryInfo from "../BaseCategoryInfo";
 
 function PlanetInfo({ name }) {
+	const hasJunction = planetsWithJunctions.includes(name);
+
 	let masteredCount = 0;
 	let masteredXP = 0;
-	let totalCount = 0;
-	let totalXP = 0;
-	const itemsMastered = useStore(
-		state =>
-			state[state.displayingSteelPath ? "steelPath" : "starChart"].filter(
-				id => nodes[name][id]
-			),
+	let totalCount = hasJunction ? 1 : 0;
+	let totalXP = hasJunction ? junctionsToXP(1) : 0;
+
+	const { nodesMastered, junctionMastered } = useStore(
+		state => ({
+			nodesMastered: state[
+				state.displayingSteelPath ? "steelPath" : "starChart"
+			].filter(id => nodes[name][id]),
+			junctionMastered:
+				hasJunction &&
+				state[
+					(state.displayingSteelPath ? "steelPath" : "starChart") +
+						"Junctions"
+				].includes(name)
+		}),
 		shallow
 	);
 	Object.entries(nodes[name]).forEach(([id, node]) => {
 		totalCount++;
 		totalXP += node.xp ?? 0;
-		if (itemsMastered.includes(id)) {
+		if (nodesMastered.includes(id)) {
 			masteredCount++;
 			masteredXP += node.xp ?? 0;
 		}
 	});
+	if (junctionMastered) {
+		masteredCount++;
+		masteredXP += junctionsToXP(1);
+	}
 
 	return (
 		<BaseCategoryInfo
