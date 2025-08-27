3469de6a25c0671e8ce50778b21fbc4f3037a10e
Neutronic <kmurphytown@gmail.com>
2025-08-26 23:10:44 -0700

    feat: backup disclaimer

--- src/components/sidebar/LinkPrompt.jsx
+++ src/components/sidebar/LinkPrompt.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
+import { useStore } from "../../hooks/useStore";
 import Button from "../Button";
 import FormInput from "../login/FormInput";
 import Select from "../Select";
-import { useStore } from "../../hooks/useStore";
 
 function LinkPrompt() {
 	const enableGameSync = useStore(state => state.enableGameSync);
@@ -43,7 +43,9 @@ function LinkPrompt() {
 			</Button>
 			{open ? (
 				<div className="popup show">
-					<div className="popup-box link-popup">
+					<div
+						className="popup-box link-popup"
+						style={{ maxWidth: "300px" }}>
 						<div className="mastery-rank">Link your account</div>
 						<FormInput
 							type="text"
@@ -59,8 +61,7 @@ function LinkPrompt() {
 								fontSize: "14px",
 								textAlign: "left",
 								paddingBottom: "10px"
-							}}
-						>
+							}}>
 							<a
 								href="https://gist.github.com/DaPigGuy/18349a0fd5ad08502305a98f8b115c26#obtaining-your-warframe-account-id"
 								target="_blank"
@@ -68,8 +69,7 @@ function LinkPrompt() {
 								style={{
 									color: "#bea966",
 									textDecoration: "underline"
-								}}
-							>
+								}}>
 								How to get Warframe account ID
 							</a>
 						</div>
@@ -89,6 +89,16 @@ function LinkPrompt() {
 								{ value: "mobile", label: "Mobile" }
 							]}
 						/>
+						<div
+							style={{
+								fontSize: "12px",
+								color: "#bea966",
+								marginTop: "10px",
+								marginBottom: "10px"
+							}}>
+							A backup of your current account will be created and
+							stored for 28 days minimum.
+						</div>
 						{error ? (
 							<div className="error-box">{error}</div>
 						) : null}
@@ -96,8 +106,7 @@ function LinkPrompt() {
 							<Button
 								centered
 								disabled={confirmDisabled}
-								onClick={onConfirm}
-							>
+								onClick={onConfirm}>
 								{loading ? (
 									<div className="spinner-small" />
 								) : (
