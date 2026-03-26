860482428dea9a15d9855428167ecd17791d4c8e
DaPigGuy <mcpepig123@gmail.com>
2026-03-26 00:57:07 -0700

    Use factions glyph as icon in planet node tooltip

--- src/components/checklist/planets/PlanetInfoTooltip.jsx
+++ src/components/checklist/planets/PlanetInfoTooltip.jsx
@@ -6,19 +6,34 @@ import {
 } from "../../../utils/nodes";
 import { PaginatedTooltipTitle } from "../../PaginatedTooltip";
 
+const factionsGlyphName = {
+	Grineer: "grineer-glyph-0232a16adc.png",
+	Corpus: "corpus-glyph-b06a192ad6.png",
+	Infested: "infestation-glyph-73b7bcff9f.png",
+	Corrupted: "orokin-glyph-7450d4cdd6.png",
+	Sentient: "",
+	Murmur: "murmur-glyph-1f38d5ee0b.png",
+	Scaldra: "scaldra-glyph-576f39c5a3.png",
+	Techrot: "techrot-glyph-217caea674.png",
+	Duviri: "",
+	Tenno: ""
+};
+
 function PlanetInfoTooltip({ node }) {
+	const factionGlyph = factionsGlyphName[factionIndexMap[node.faction]];
+
 	return (
 		<div className="item-tooltip">
 			<PaginatedTooltipTitle title="General Information" />
 			<span className="mission-info">
 				Level {node.lvl.join("-")}{" "}
-				<img
-					className="faction-icon"
-					src={`https://cdn.warframestat.us/genesis/svg/factions/${factionIndexMap[
-						node.faction
-					].toLowerCase()}.svg`}
-					alt=""
-				/>
+				{factionGlyph && (
+					<img
+						className="faction-icon"
+						src={`https://cdn.warframestat.us/img/${factionGlyph}`}
+						alt=""
+					/>
+				)}
 				{factionIndexMap[node.faction]} {missionIndexMap[node.type]}
 			</span>
 			<span>{node.xp ?? 0} XP</span>
@@ -31,3 +46,4 @@ PlanetInfoTooltip.propTypes = {
 };
 
 export default PlanetInfoTooltip;
+
